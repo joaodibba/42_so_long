4cfeb583df2fee0b6be79d9c29297a2200042cde
joaodibba <joaomiranda.pessoal@gmail.com>
2023-05-06 18:47:59 +0100

    update

--- src/verify_map.c
+++ src/verify_map.c
@@ -6,7 +6,7 @@
 /*   By: jalves-c < jalves-c@student.42lisboa.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 03:45:59 by jalves-c          #+#    #+#             */
-/*   Updated: 2023/05/06 18:43:42 by jalves-c         ###   ########.fr       */
+/*   Updated: 2023/05/06 18:47:45 by jalves-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,20 +22,31 @@ int	map_check(char *path_to_map)
 	return (EXIT_SUCCESS);
 }
 
-void	get_map_dimensions(t_map *map)
-{
-	size_t	i;
-
-	i = 0;
-	*(map->cols) = ft_strlen(map->map[0]);
-	while(map->map[i] != NULL)
-		i++;
-	*(map->rows) = i;
+void get_map_dimensions(t_map *map) {
+    size_t i = 0;
+    if (map->cols == NULL) {
+        map->cols = malloc(sizeof(*(map->cols)));
+        if (map->cols == NULL) {
+            // handle error: unable to allocate memory
+            return;
+        }
+    }
+    *(map->cols) = ft_strlen(map->map[0]);
+    while (map->map[i] != NULL) {
+        i++;
+    }
+    if (map->rows == NULL) {
+        map->rows = malloc(sizeof(*(map->rows)));
+        if (map->rows == NULL) {
+            // handle error: unable to allocate memory
+            return;
+        }
+    }
+    *(map->rows) = i;
 }
-
 bool	map_validator(t_map *map)
 {
-	get_map_dimensions(&map);
+	get_map_dimensions(map);
 	ft_printf("colunas %d", map->cols);
 	ft_printf("linhas %d", map->rows);
 	(void)map;
